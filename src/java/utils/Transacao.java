856c927d906b8d1b175a2ef075043dfe5520b7e4
Lucas <Lucas@10.148.84.206>
2016-12-02 20:02:28 -0200

    teste

--- src/java/utils/Transacao.java
+++ src/java/utils/Transacao.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package utils;
 
 /**
  *
  * @author Lucas
  */
+package utils;
+
+import java.sql.*;
+
 public class Transacao {
-    
-}
+
+  // connection data
+  static final String ODBC_DRIVER = "sun.jdbc.odbc.JdbcOdbcDriver";
+  static final String DSN = "jdbc:odbc:pmr2490";
+  static final String USER = "admin";
+  static final String PWD = "";
+
+  private Connection _conexao = null;
+  private boolean _readOnly = false;
+
+  public void begin() throws Exception{
+      Class.forName(ODBC_DRIVER).newInstance();
+      _conexao = DriverManager.getConnection(DSN,USER,PWD);
+      _conexao.setAutoCommit(false);
+	  _readOnly = false;
+  } // begin
+
+  public void beginReadOnly() throws Exception{
+      Class.forName(ODBC_DRIVER).newInstance();
+      _conexao = DriverManager.getConnection(DSN,USER,PWD);
+	  _readOnly = true;
+  } // begin
+
+  public void commit() throws Exception {
+      if ( !_readOnly) {
+         _conexao.commit();
+	  }
+	  _conexao.close();
+  } // commit
+
+  public void rollback() throws Exception {
+      if ( !_readOnly) {
+         _conexao.rollback();
+	  }
+	  _conexao.close();
+  } // rollback
+
+  public Connection obterConexao() {
+      return _conexao;
+  } // obterConexao
+
+} // Transacao
