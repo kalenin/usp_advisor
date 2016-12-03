41fad615a7879f7c45a5a4a4a730446260419652
Lucas Guardia Palopoli <palopolilucas@gmail.com>
2016-12-03 18:47:43 -0200

    nomes das classes

--- src/java/data/evaluationsDO.java
+++ src/java/data/evaluationsDO.java
@@ -9,7 +9,7 @@ package java.data;
  *
  * @author Lucas
  */
-public class evaluationsDO {
+public class EvaluationsDo {
     private int id;
     private int offering_id;
     private int student_id;
